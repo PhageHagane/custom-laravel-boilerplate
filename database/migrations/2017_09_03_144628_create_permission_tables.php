9ba61e4c9a0ad31074d80e8016861084c50a957c
phage <danalfreifuerte@gmail.com>
2025-03-12 11:43:14 +0800

    update permissions module

--- database/migrations/2017_09_03_144628_create_permission_tables.php
+++ database/migrations/2017_09_03_144628_create_permission_tables.php
@@ -16,7 +16,6 @@ class CreatePermissionTables extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('guard_name');
-            $table->string('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
